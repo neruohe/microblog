e7ea9e7086fa4f1182bdea600d0a3830c3e01024
neruohe <neruohe@gmail.com>
2016-05-23 20:47:38 +0800

    set database migrations

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
+SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/microblog'
+SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 OPENID_PROVIDERS = [
     { 'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id' },
